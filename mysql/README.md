b5e5bd0e8cff9192e611cd2d88632ab0902b7a38
godspeedcurry <3160100421@zju.edu.cn>
2022-09-21 17:02:52 +0800

    first add

---
+++ mysql/README.md
@@ -0,0 +1,6 @@
+重新build
+```
+docker-compose down
+docker volume prune 
+docker-compose up -d --build
+```
--- mysql/startup/init/init.sql
+++ mysql/startup/init/init.sql
@@ -1,11 +1,11 @@
-
-drop database if exists users;
-create database users;
-use users;
+use ctf;
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(256) CHARACTER SET utf8mb4 NOT NULL UNIQUE COMMENT '用户名',
-  `password` varchar(256) CHARACTER SET utf8mb4 DEFAULT NULL UNIQUE COMMENT '密码'
+  `password` varchar(256) CHARACTER SET utf8mb4 DEFAULT NULL UNIQUE COMMENT '密码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
+
+insert into users values(1,'admin','e10adc3949ba59abbe56e057f20f883e');
+insert into users values(2,'test','fcea920f7412b5da7be0cf42b8c93759');
