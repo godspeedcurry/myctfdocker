0f15720ac11730dc2c396476e0f30dcc1ecd6e15
godspeedcurry <3160100421@zju.edu.cn>
2023-05-03 14:20:57 +0800

    add php

--- mysql/startup/init/init.sql
+++ mysql/startup/init/init.sql
@@ -1,4 +1,4 @@
-use ctf;
+use db;
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -7,5 +7,3 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
 
-insert into users values(1,'admin','e10adc3949ba59abbe56e057f20f883e');
-insert into users values(2,'test','fcea920f7412b5da7be0cf42b8c93759');
